d1edf681f09a51b88c706a7eafe3578783a865ae
el-schneider <mail@jonaslist.de>
2026-04-03 01:52:22 +0200

    fix: reviewer fixes for translation service stubs

--- src/Services/PrismTranslationService.php
+++ src/Services/PrismTranslationService.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace ElSchneider\ContentTranslator\Services;
 
 use ElSchneider\ContentTranslator\Contracts\TranslationService;
+use RuntimeException;
 
 final class PrismTranslationService implements TranslationService
 {
     public function translate(array $units, string $sourceLocale, string $targetLocale): array
     {
-        throw new \RuntimeException('Not implemented');
+        throw new RuntimeException('Not implemented');
     }
 }
