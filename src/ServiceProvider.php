84720ab09dedb9d23659dc87cd9f81b213df2609
el-schneider <mail@jonaslist.de>
2026-04-02 21:26:58 +0200

    initial addon scaffold

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ElSchneider\ContentTranslator;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
