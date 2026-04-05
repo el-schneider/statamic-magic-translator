3a01f538944be905579d66bdb4774609469b39c2
el-schneider <mail@jonaslist.de>
2026-04-05 09:40:03 +0200

    fix: ship v5 animate-spin via dedicated css entry instead of js injection

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/v5/addon.ts', 'resources/js/v6/addon.ts'],
+            input: ['resources/js/v5/addon.ts', 'resources/js/v5/addon.css', 'resources/js/v6/addon.ts'],
             publicDirectory: 'resources/dist',
         }),
         statamic(),
